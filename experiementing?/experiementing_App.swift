2c92f344e58fe35d0abb437e831f7edf003cff22
zi qi <jiangzziqi@gmail.com>
2023-07-22 10:12:29 +0800

    Initial Commit

---
+++ experiementing?/experiementing_App.swift
@@ -0,0 +1,17 @@
+//
+//  experiementing_App.swift
+//  experiementing?
+//
+//  Created by T Krobot on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct experiementing_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
